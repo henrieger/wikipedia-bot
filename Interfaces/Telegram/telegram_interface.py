3cd55695a1cac0fe39a3d934db58c88a340aab10
Henrique Rieger <henriquerieger2001@gmail.com>
2021-12-26 02:49:32 -0300

    [add] - basic Telegram interface

---
+++ Interfaces/Telegram/telegram_interface.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+# BASED ON BOT FOUND IN https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/echobot.py
+
+import sys
+sys.path.append('/home/user/Git-Projects/wikipedia-bot/')
+
+import logging
+import os
+import wiki
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, MessageFilter, CallbackContext
+from dotenv import load_dotenv
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+class FilterWikipedia(MessageFilter):
+    def filter(self, message):
+        return wiki.has_wiki_article(message.text)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    # Send a message when the command /start is issued.
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    # Send a message when the command /help is issued.
+    update.message.reply_text('Help!')
+
+def reply_with_resume(update: Update, context: CallbackContext) -> None:
+    bot_message = '\n<i>Beep boop, I\'m a bot. You can find my source code <a href="https://github.com/henrieger/wikipedia-bot">here</a>.</i>'
+    wiki_link = wiki.get_wiki_article(update.message.text)
+    wiki_content = wiki.api_query(wiki_link)
+    update.message.reply_html(wiki.format_response(wiki_content, type='html', domain=wiki_link)+bot_message,
+    disable_web_page_preview=True)
+
+def main() -> None:
+    # Start the bot
+
+    # Create the Updater and pass it your bot's token.
+    load_dotenv()
+    token = os.getenv('TELEGRAM_TOKEN')
+    updater = Updater(token)
+
+    # Create instance of custom Wikipedia filter
+    wiki_filter = FilterWikipedia()
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on message containing Wiki link - answer with article resume
+    dispatcher.add_handler(MessageHandler(wiki_filter, reply_with_resume))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
