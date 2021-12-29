787d04c230f1ef44f917407ceba69cb6aaec994e
Henrique Rieger <henriquerieger2001@gmail.com>
2021-12-29 20:10:22 -0300

    [add] search command to help message

--- Interfaces/bot_messages.py
+++ Interfaces/bot_messages.py
@@ -1,10 +1,11 @@
-start_html ="""I'm the Wikipedia Bot. Send me a message with a link to a Wikipedia article and I'll give you the first paragraph of the article. You can also add me to a group and I'll answer any message like that in there too.
+start_html ="""I'm the Wikipedia Bot. Send me a message with a link to a Wikipedia article or '/search {term}' and I'll give you the first paragraph of the article. You can also add me to a group and I'll answer any message like that in there too.
 
 <i>This bot was made by @henrieger. You can find the source code <a href="https://github.com/henrieger/wikipedia-bot">here</a>.</i>"""
 
 help_text = """To use this bot, simply send a message in a group where I am with a link to a Wikipedia article and I'll reply with the first paragraph of the article.
 
 Other useful commands are:
+    /search - Search for term in Wikipedia. Usage - /search {term} (lang={language})
     /start - Give a start message with relevant info of the bot.
     /help - Reply with a message of how to use the bot."""
 
