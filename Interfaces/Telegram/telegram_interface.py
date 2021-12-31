ee393e235223ace94e5476c1e81570869e9b8624
Henrique Rieger <henriquerieger2001@gmail.com>
2021-12-31 09:35:30 -0300

    [fix] language change from en to en

--- Interfaces/Telegram/telegram_interface.py
+++ Interfaces/Telegram/telegram_interface.py
@@ -71,13 +71,13 @@ def search(update: Update, context: CallbackContext) -> None:
     # Get content based on pageid search result
     search_term = re.sub(r'\/search\@?\w* ', '', message_text)
     wiki_id = wiki.search_result(search_term, lang=lang)
-    if wiki_id == -1:
+    if wiki_id == -1 and lang != 'en':
         update.message.reply_html(bot_messages.not_found_in_lang_html(search_term, lang))
         lang = 'en'
         wiki_id = wiki.search_result(search_term, lang=lang)
-        if wiki_id == -1:
-            update.message.reply_html(bot_messages.not_found_text+'\n'+bot_messages.im_a_bot_html, disable_web_page_preview=True)
-            return
+    if wiki_id == -1:
+        update.message.reply_html(bot_messages.not_found_text+'\n'+bot_messages.im_a_bot_html, disable_web_page_preview=True)
+        return
     wiki_content = wiki.api_query_id(wiki_id, lang=lang)
 
     # Generate final message
